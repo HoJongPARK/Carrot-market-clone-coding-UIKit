949e4aab0fa6b99e2935c517bfc8896e679fc6b7
HoJong <pjh00098@gmail.com>
2022-05-29 16:51:58 +0900

    Feat: Add Onboarding page 3

---
+++ CarrotMarket/Constant/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  CarrotMarket
+//
+//  Created by JongHo Park on 2022/05/29.
+//
+
+import Foundation
+
+enum Constant {
+  enum ImageName {
+    static let ON_BOARDING_START_IMAGE = "OnboardingImage"
+    static let LOCK_DANGUENE = "LockDanguene"
+  }
+}
